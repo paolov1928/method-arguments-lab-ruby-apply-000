4f31dfccf38428fb531cc528568013fea713c603
ventura.paolo <ventura.paolo@gmail.com>
2018-09-19 22:07:13 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,14 @@
+#introduction
+  #takes in an argument of a name and puts out a phrase with that name using string interpolation (FAILED - 1)
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+  
+
+
+#introduction_with_language
+  #takes in two arguments, a name and a language, and puts out #a phrase using those arguments. (FAILED - 2)
+
+def introduction_with_language(name, language)
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+ end
