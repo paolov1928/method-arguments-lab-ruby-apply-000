1bd1f2d6856e0ac65e4b135129352770c8ab2378
Paolo Ventura <ventura.paolo@gmail.com>
2018-09-22 13:19:52 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,24 @@
 #introduction
   #takes in an argument of a name and puts out a phrase with that name using string interpolation (FAILED - 1)
 def introduction(name)
+<<<<<<< HEAD
   puts "Hi, my name is #{name}."
 end
+=======
+  "hi #{name}"
+>>>>>>> 658334d6846e8e78059ba214091d9005acd9c61f
   
 
 
 #introduction_with_language
   #takes in two arguments, a name and a language, and puts out #a phrase using those arguments. (FAILED - 2)
+<<<<<<< HEAD
 
 def introduction_with_language(name, language)
  puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
+=======
+  
+  def introduction_with_language(name, language)
+    "hi"
+>>>>>>> 658334d6846e8e78059ba214091d9005acd9c61f
